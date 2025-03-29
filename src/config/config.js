5e19ec38393df1051ec1a4e0419d548a730f424c
arm_nugraha <armnugraha@envisions.app>
2025-03-29 09:04:07 +0700

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@
 const config = {
   // Meta Information
   meta: {
-    title: "Febri & Arman Wedding",
+    title: "Febri & Arman",
     description: "We are getting married and would love for you to be a part of our celebration.",
     ogImage: "/images/og-image.jpg", // OpenGraph image for social media
     favicon: "/images/favicon.ico",
@@ -11,7 +11,11 @@ const config = {
 
   couple: {
     groomName: "Febri",
+    groomFullName: "Febriati Dian Mubarokah",
+    groomParents: "Bapak Suratno dan Ibu Isnainiyah",
     brideName: "Arman",
+    brideFullName: "Arman Nugraha",
+    brideParents: "Bapak Deni Andrian dan Ibu Popon Kurniati",
   },
 
   // Event Details
@@ -19,25 +23,25 @@ const config = {
     date: "2025-06-28",
     time: "16:22",
     timezone: "WIB",
-    dateTime: "2025-06-28T00:00:00Z", // ISO 8601 format
-    name: "Puri Eka Bachtiar",
-    address: "Jl. Raya Peusar, Sukamulya, Kec. Cikupa, Kabupaten Tangerang, Banten 15710",
-    time: "07:00 - 13:00 WIB",
+    dateTime: "2025-06-28T01:00:00Z", // ISO 8601 format
+    name: "Keboen Djati Diri",
+    address: "Jl. AMD Desa Rancakalapa, Panongan, Kec. Panongan, Kabupaten Tangerang, Banten 15710",
+    time: "08:00 - 13:00 WIB",
     phone: "+62 123 4567 890",
-    maps_url: "https://maps.google.com/?q=Puri Eka Bachtiar",
-    maps_embed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.1664710111863!2d106.51208607499869!3d-6.241779161120018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e42070f450ac91f%3A0x81ee26571497d90b!2sPuri%20Eka%20Bachtiar!5e0!3m2!1sid!2sid!4v1740283475560!5m2!1sid!2sid",
-    direction: "https://www.google.com/maps?saddr=My+Location&daddr=-6.2417792,106.5120861",
-    latitude: -6.2417792, // Replace with actual coordinates
-    longitude: 106.5120861 // Replace with actual coordinates
+    maps_url: "https://maps.google.com/?q=Keboen Djati Diri",
+    maps_embed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.88390773576!2d106.51794047499914!3d-6.2789901614632555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e4207221c760d65%3A0xf0b9fe1a999bb008!2sKeboen%20Djati%20Diri!5e0!3m2!1sid!2sid!4v1743213495336!5m2!1sid!2sid",
+    direction: "https://www.google.com/maps?saddr=My+Location&daddr=-6.2789902,106.5179405",
+    latitude: -6.2789902, // Replace with actual coordinates
+    longitude: 106.5179405 // Replace with actual coordinates
   },
 
   eventDetails: [{
     title: "Akad Nikah",
     date: "2025-06-28",
-    startTime: "07:00",
+    startTime: "08:00",
     endTime: "09:00",
     timeZone: "Asia/Jakarta",
-    location: "Puri Eka Bachtiar",
+    location: "Keboen Djati Diri",
     description: "We invite you to join us in celebrating our wedding ceremony."
   }, {
     title: "Resepsi Nikah",
@@ -45,7 +49,7 @@ const config = {
     startTime: "10:00",
     endTime: "13:00",
     timeZone: "Asia/Jakarta",
-    location: "Puri Eka Bachtiar",
+    location: "Keboen Djati Diri",
     description: "We invite you to join us in celebrating our wedding ceremony."
   }],
 
